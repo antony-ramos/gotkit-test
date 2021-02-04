0965cff4f74cb19fcca6ad5fac66370c4a023ad2
Your Name <you@example.com>
2021-02-04 15:39:42 +0100

    add github integration

---
+++ test.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+
+	httptransport "github.com/go-kit/kit/transport/http"
+)
+
+type uppercaseRequest struct {
+	S string `json:"s"`
+}
+
+type uppercaseResponse struct {
+	V   string `json:"v"`
+	Err string `json:"err,omitempty"` // errors don't JSON-marshal, so we use a string
+}
+
+type countRequest struct {
+	S string `json:"s"`
+}
+
+type countResponse struct {
+	V int `json:"v"`
+}
+
+type Endpoint func(ctx context.Context, request interface{}) (response interface{}, err error)
+
+func main() {
+	svc := stringService{}
+
+	uppercaseHandler := httptransport.NewServer(
+		makeUppercaseEndpoint(svc),
+		decodeUppercaseRequest,
+		encodeResponse,
+	)
+
+	countHandler := httptransport.NewServer(
+		makeCountEndpoint(svc),
+		decodeCountRequest,
+		encodeResponse,
+	)
+
+	http.Handle("/uppercase", uppercaseHandler)
+	http.Handle("/count", countHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
